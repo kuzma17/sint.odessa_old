44f9bca250bc0b08a50214ea22c9eb186f8161ba
kuzma <kuzma@maill.ru>
2017-04-26 09:13:53 +0300

    Search site

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,15 +4,35 @@ namespace App\Http\Controllers;
 
 use App\News;
 use App\Page;
+use App\Post;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function search($query){
-        $page_search = Page::search($query)->get();
-        $news_search = News::search($query)->get();
+        $page = json_decode(Page::search($query)->get());
+
+        $news = json_decode(News::search($query)->get());
+        //$url_hews = 'news/'.$news->id;
+
+        //echo $url_hews;
+        //$news['url'] = $url_hews;
+
+        var_dump($news);
+
+        //$post = json_decode(Post::search($query)->get());
+        //$url_post = 'news/'.$post->id;
+        //$post['url'] = $url_post;
+
        // $res = array_merge($page_search, $news_search);
-        $results = json_decode( $news_search);
+        //$results = json_decode( $news_search);
+
+        $results = array_merge(
+            $page,
+            $news
+           // $post
+        );
+
         return view('search', ['results'=>$results]);
     }
 }
